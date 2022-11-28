086b1eff1bb55915e1c632c524cc4db7fd1e82b8
wamas <wamas@LUNANIA>
2022-11-28 12:58:30 +0100

    Boat is boat

--- Project22GR2/Models/Boat.cs
+++ Project22GR2/Models/Boat.cs
@@ -1,6 +1,27 @@
-﻿namespace Project22GR2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+
+namespace Project22GR2.Models
 {
     public class Boat
     {
+
+        [Required]
+        [Range(typeof(int), "1", "1000", ErrorMessage = "Error. ID is outside of the interval.")]
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public string Sail { get; set; }
+
+        public Boat()
+        {
+
+        }
+
     }
 }
