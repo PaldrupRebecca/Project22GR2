cb2b69c83b0dc3c66aa45d554bfe10155d7a83b4
wamas <wamas@LUNANIA>
2022-11-29 10:30:24 +0100

    tweaks to boat model

--- Project22GR2/Models/Boat.cs
+++ Project22GR2/Models/Boat.cs
@@ -12,8 +12,12 @@ namespace Project22GR2.Models
 
         public string Type { get; set; }
 
+        Required]
+        [Range(typeof(double), "0.1", "1000", ErrorMessage = "Error. Length is outside of the interval.")]
         public double Length { get; set; }
 
+        Required]
+        [Range(typeof(double), "0.1", "1000", ErrorMessage = "Error. Width is outside of the interval.")]
         public double Width { get; set; }
 
         public string Sail { get; set; }
