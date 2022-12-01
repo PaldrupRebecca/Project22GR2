d10506d585cd0d54488a47fad16615e1c58340ed
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-01 09:55:22 +0100

    Class + repo

---
+++ Project22GR2/Interfaces/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using Project22GR2.Models;
+
+namespace Project22GR2.Interfaces
+{
+    public interface IEventRepository
+    {
+        List<Event> GetAllEvents();
+        Event GetEvent(int id);
+        void AddEvent(Event ev);
+        void UpdateEvent(Event ev);
+        void DeleteEvent(int id);
+        List<Event> FilterEvents(string filter);
+    }
+}
