4b88d00dc8874e95c098caf40c1e9ad951287a63
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-01 10:03:32 +0100

    Json

---
+++ Project22GR2/Helpers/JsonFileReaderEvents.cs
@@ -0,0 +1,17 @@
+﻿using Project22GR2.Models;
+using System.Text.Json;
+
+namespace Project22GR2.Helpers
+{
+    public class JsonFileReaderEvents
+    {
+        public static List<Event> ReadJsonEvents(string jsonFileName)
+        {
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                string indata = jsonFileReader.ReadToEnd();
+                return JsonSerializer.Deserialize<List<Event>>(indata);
+            }
+        }
+    }
+}
---
+++ Project22GR2/Helpers/JsonFileWriterEvents.cs
@@ -0,0 +1,22 @@
+﻿using Project22GR2.Models;
+using System.Text.Json;
+
+namespace Project22GR2.Helpers
+{
+    public class JsonFileWriterEvents
+    {
+        public static void WritetoJsonEvents(List<Event> events, string jsonFileName)
+        {
+            //using(FileStream outputStream =File.OpenWrite(jsonFileName))
+            using (FileStream outputStream = File.Create(jsonFileName))
+            {
+                var writter = new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = false,
+                    Indented = true,
+                });
+                JsonSerializer.Serialize<Event[]>(writter, events.ToArray());
+            }
+        }
+    }
+}
