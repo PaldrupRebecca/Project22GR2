79e4bd4d3c94c3d3fa12969c772a0717d10c0c83
Adam Nørrind Schaumburg-Müller <adam8154@edu.zealand.dk>
2022-12-01 10:54:44 +0100

    Member

---
+++ Project22GR2/Interfaces/IMemberRepository.cs
@@ -0,0 +1,19 @@
+﻿using Project22GR2.Models;
+
+namespace Project22GR2.Interfaces
+{
+    public interface IMemberRepository
+    {
+        List<Member> GetAllMembers();
+        Member GetMember(int id);
+
+        void AddMember(Member members);
+
+        void UpdateMember(Member members);
+
+        void DeleteMember(int id);
+
+        List<Member> FilterMember(string filter);
+
+    }
+}
