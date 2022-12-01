746d901c1aaa60796a847cf766e5d7399c6febab
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-01 12:23:09 +0100

    Delete event added

---
+++ Project22GR2/Pages/Events/DeleteEvent.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project22GR2.Interfaces;
+using Project22GR2.Models;
+
+namespace Project22GR2.Pages.Events
+{
+    public class DeleteEventModel : PageModel
+    {
+        private IEventRepository _repo;
+
+        [BindProperty]
+        public Event Event { get; set; }
+
+        public DeleteEventModel(IEventRepository eventRepository)
+        {
+            _repo = eventRepository;
+        }
+
+        public void OnGet(int id)
+        {
+            Event = _repo.GetEvent(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            _repo.DeleteEvent(Event.Id);
+            return RedirectToPage("Index");
+        }
+    }
+}
