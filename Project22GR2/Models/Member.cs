885dbe22ee126358d7dabb1c256080aec579d047
Adam Nørrind Schaumburg-Müller <adam8154@edu.zealand.dk>
2022-12-01 09:32:18 +0100

    Employee

--- Project22GR2/Models/Member.cs
+++ Project22GR2/Models/Member.cs
@@ -2,5 +2,18 @@
 {
     public class Member
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public Member(int id, string name, string address, string email)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Email = email;
+        }
     }
 }
