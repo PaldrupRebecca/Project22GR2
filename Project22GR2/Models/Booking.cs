16919284b27b49c178ab9de9560149942b528876
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-02 09:25:10 +0100

    Booking properties

--- Project22GR2/Models/Booking.cs
+++ Project22GR2/Models/Booking.cs
@@ -2,5 +2,10 @@
 {
     public class Booking
     {
+        //Rebecca
+        public int Id { get; set; }
+        public Boat Boat { get; set; }
+        public Member Member { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
