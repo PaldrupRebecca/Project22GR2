766088c5a0f15cc44872a50a93316ebbda679c4b
Daniel Hass <dani67h0@edu.zealand.dk>
2022-12-05 09:17:25 +0100

    123

--- Project22GR2/Models/BlogPost.cs
+++ Project22GR2/Models/BlogPost.cs
@@ -2,5 +2,28 @@
 {
     public class BlogPost
     {
-    }
+		private string _name;
+        private int _id;
+        
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public int Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+		public DateTime EndDate { get; private set; }
+
+		public BlogPost(string name, int id)
+		{
+			_name = name;
+			_id = id;
+			EndDate = DateTime.Now;
+
+		}
+
+	}
 }
