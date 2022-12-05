98b54d64f4b731d2c74e77705d6f8e7b0126b1d7
wamas <wamas@LUNANIA>
2022-12-05 12:55:29 +0100

    Started working on Login page, as of right now only Employees can log in

---
+++ Project22GR2/Pages/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project22GR2.Services;
+using Project22GR2.Interfaces;
+using Project22GR2.Models;
+
+namespace Project22GR2.Pages
+{
+    // Luca
+    // AS OF THIS CURRENT BUILD, ONLY EMPLOYEES CAN LOG IN
+
+    public class LoginModel : PageModel
+    {
+        private IEmployeeRepository repo;
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public LoginModel(IEmployeeRepository faemRepo)
+        {
+            repo = faemRepo;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            // Make a new employee, this is purposefully empty
+            Employee placeholderE = null;
+
+            // Looks for email in database
+            foreach (Employee e in repo.GetAllEmployees())
+            {
+                if (e.Email == Employee.Email)
+                {
+                    placeholderE = e;
+                    break;
+                }
+                else
+                    return Page();
+            }
+
+            // Checks password
+            if (placeholderE.Password == Employee.Password)
+            {
+                // Success
+                return RedirectToPage("Index");
+            }
+            else
+                // No dice
+                return Page();
+        }
+    }
+}
