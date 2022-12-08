20fde41c65bd8459028cda7db42d984a75c646e3
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-08 11:16:35 +0100

    small fix

--- Project22GR2/Services/JsonJoinEventRepository.cs
+++ Project22GR2/Services/JsonJoinEventRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace Project22GR2.Services
+﻿using Project22GR2.Helpers;
+using Project22GR2.Interfaces;
+using Project22GR2.Models;
+
+namespace Project22GR2.Services
 {
-    public class JsonJoinEventRepository
+    public class JsonJoinEventRepository : IJoinEventRepository
     {
+        string jsonFileName = @"Data\JsonJoinEvent.json";
+
+        public void AddJoinEvent(JoinEvent joinEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteJoinEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<JoinEvent> FilterJoinEvents(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<JoinEvent> GetAllJoinEvents()
+        {
+            return JsonFileReader.ReadJsonJoinEvent(jsonFileName);
+        }
+
+        public JoinEvent GetJoinEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateJoinEvent(JoinEvent joinEvent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
