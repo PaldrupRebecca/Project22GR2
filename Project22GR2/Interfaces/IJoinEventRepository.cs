22d3801418366878c2bffc6b264141cdd113652c
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-08 11:05:52 +0100

    IJoinEventRepository

--- Project22GR2/Interfaces/IJoinEventRepository.cs
+++ Project22GR2/Interfaces/IJoinEventRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Project22GR2.Interfaces
+﻿using Project22GR2.Models;
+
+namespace Project22GR2.Interfaces
 {
     public interface IJoinEventRepository
     {
+        List<JoinEvent> GetAllJoinEvents();
+        JoinEvent GetJoinEvent(int id);
+        void AddJoinEvent(JoinEvent joinEvent);
+        void UpdateJoinEvent(JoinEvent joinEvent);
+        void DeleteJoinEvent(int id);
+        List<JoinEvent> FilterJoinEvents(string filter);
     }
 }
