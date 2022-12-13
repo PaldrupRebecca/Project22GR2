e495b9ec1f64e9c0813b4c6d89e6b3d61d52b0ab
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-13 10:58:05 +0100

    See event created

---
+++ Project22GR2/Pages/Events/SeeEvent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Project22GR2.Pages.Events
+{
+    public class SeeEventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
