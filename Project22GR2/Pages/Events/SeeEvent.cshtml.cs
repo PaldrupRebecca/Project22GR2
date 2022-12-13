92877810394e7a6ba7fa16122aec4f26d49d794a
Rebecca Maria Børsen Paldrup <rebe2907@edu.zealand.dk>
2022-12-13 11:17:06 +0100

    dunno

--- Project22GR2/Pages/Events/SeeEvent.cshtml.cs
+++ Project22GR2/Pages/Events/SeeEvent.cshtml.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project22GR2.Interfaces;
+using Project22GR2.Models;
 
 namespace Project22GR2.Pages.Events
 {
     public class SeeEventModel : PageModel
     {
-        public void OnGet()
+        private IEventRepository _repo;
+
+        [BindProperty]
+        public Event Event { get; set; }
+        [BindProperty]
+        public JoinEvent JoinEvent { get; set; }
+
+        public SeeEventModel(IEventRepository eventRepository)
+        {
+            _repo = eventRepository;
+        }
+
+        public void OnGet(int id)
+        {
+            Event = _repo.GetEvent(id);
+        }
+
+        public void OnPost()
         {
         }
     }
